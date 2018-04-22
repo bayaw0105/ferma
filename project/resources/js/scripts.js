670832151c8fa804d27e04e8a93a4029cbcb04e9
Baiaman <bayaw0105@gmail.com>
2018-04-22 21:52:16 +0700

    Site developed ready on work 21:52

--- project/resources/js/scripts.js
+++ project/resources/js/scripts.js
@@ -1,33 +1,69 @@
 $(document).ready(function () {
+
+    $(window).scroll( function(){
+        if ($(this).scrollTop() > $(this).height()){
+            $('.top').addClass('active');
+        } else {
+            $('.top').removeClass('active');
+        }
+        
+        if($(this).scrollTop() > $(this).height()){
+            $('.top-header').addClass('top-header-white');
+        } else {
+            $('.top-header').removeClass('top-header-white');
+        }
+    });
     
+    
+
+    /* Navigation scroll */
+    $(document).ready(function () {
+        $('a[href*=#]').bind('click', function (e) {
+            e.preventDefault();
+
+            var target = $(this).attr("href");
+
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+            }, 1000, function () {
+                location.hash = target;
+            });
+
+            return false;
+        });
+    });
+
+    /*menu*/
+
     $('#menu-btn').click(function () {
         $('nav').slideToggle(900);
         if ($(this).hasClass('not-active')) {
             $(this).addClass('is-active').removeClass('not-active');
-        } 
-        else {
+        } else {
             setTimeout(function () {
                 $('.is-active').addClass('not-active').removeClass('is-active')
             }, 600)
         }
     });
-    
-    /*google map*/
-    
+
+     
     var map = new GMaps({
-        div: '.map',
-        lat: 42.8754806,
-        lng: 74.5886487
-    });
-    
-    map.addMarker({
-        lat: 42.8754806,
-        lng: 74.5886487,
-        title: 'Title text',
-        infoWindow: {
-            content: '<p>Title text</p>'
-        }
-    });
-    
+		div: '.map',
+		lat: 42.843887,
+		lng: 74.269880
+	});
+	
+	map.addMarker({ 
+		lat: 42.843887,
+		lng: 74.269880,
+		title: 'ОАО «АК-КУУ»',
+		infoWindow: {
+			content: '<p>ОАО «АК-КУУ»</p>'
+		}
+	});
+
+
+
+  
 
 })
